f5173d6800b001cddf248e19e1657622b3b40fc5
kangdongkil <kbk3418@naver.com>
2014-04-17 22:31:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 	$('#up-btn').click(function(){
+		$('#player').animate({top:'-=10px'},1000);
+	});
+	$('#down-btn').click(function(){
 		$('#player').animate({top:'+=10px'},1000);
-		
+	});
+	$('#left-btn').click(function(){
+		$('#player').animate({left:'-=10px'},1000);
+	});
+	$('#right-btn').click(function(){
+		$('#player').animate({left:'+=10px'},1000);
 	});
 
 });
