667b2592e3665fab090fa232dc775ec964ff4a91
kangdongkil <kbk3418@naver.com>
2014-04-17 22:32:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 	$('#up-btn').click(function(){
-		$('#player').animate({top:'-=10px'},1000);
+		$('#player').animate({top:'-=100px'},1000);
 	});
 	$('#down-btn').click(function(){
-		$('#player').animate({top:'+=10px'},1000);
+		$('#player').animate({top:'+=100px'},1000);
 	});
 	$('#left-btn').click(function(){
-		$('#player').animate({left:'-=10px'},1000);
+		$('#player').animate({left:'-=100px'},1000);
 	});
 	$('#right-btn').click(function(){
-		$('#player').animate({left:'+=10px'},1000);
+		$('#player').animate({left:'+=100px'},1000);
 	});
 
 });
